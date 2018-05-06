3d30b024118149e790d637590ada3e8177f5754f
Grantjjscott <grant.j.j.scott>
2018-05-06 13:12:52 -0400

    6.0
    added order list to cook gui

--- app/src/main/java/com/example/grant/restaur_auto/ServerActivity.java
+++ app/src/main/java/com/example/grant/restaur_auto/ServerActivity.java
@@ -1,22 +1,31 @@
 package com.example.grant.restaur_auto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ViewFlipper;
 import android.*;
 
-public class ServerActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class ServerActivity extends AppCompatActivity {
+    private ArrayAdapter<String> adapter;
+    ArrayList<String> items;
+    int i;
     public void newOrder(View view){
-    MainActivity.server01.newOrder(01, null);
+    Intent order = new Intent(this, NewOrderActivity.class);
+    startActivity(order);
     }
-S
+
     public void printRepceit(View view){
-        I
-        //order.generateReceipt();
+    Intent repceit = new Intent(this, Pop.class);
+        startActivity(repceit);
+    //order.generateReceipt();
     }
 
     public void additems(View view){
@@ -38,6 +47,22 @@ S
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+        // get list view
+        ListView listView = (ListView) findViewById(R.id.listitem);
+        // menu to string
+        items = new ArrayList<>();
+        i= 0;
+        while (i< MainActivity.menu.size()) {
+            items.add(MainActivity.menu.get(i).toString());
+            i++;
+
+        }
+        // write menu
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, items);
+        listView.setAdapter(adapter);
+
     }
+    //TODO onclick listener for all buttons
+
 
 }
---
+++ app/src/main/java/com/example/grant/restaur_auto/TableActivity.java
@@ -0,0 +1,39 @@
+package com.example.grant.restaur_auto;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import static java.security.AccessController.getContext;
+
+public class TableActivity extends AppCompatActivity {
+    private ArrayAdapter<Table> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+        ListView listView = (ListView)findViewById(R.id.ListView);
+<<<<<<< HEAD
+
+=======
+>>>>>>> d497fc46d9b4effca65bdcae52d12ebe712bd6b5
+         adapter = new ArrayAdapter<Table>(this,android.R.layout.simple_expandable_list_item_1,MainActivity.tables);
+
+        listView.setAdapter(adapter);
+    }
+
+    public void addTable(View view){
+        Intent newTable = new Intent(this, CreateTableActivity.class);
+        startActivity(newTable);
+    }
+
+
+    }
